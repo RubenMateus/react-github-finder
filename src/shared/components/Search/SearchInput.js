f8fa4c257452d9c0fbd9658f4ba8b1d3597ab2fd
Restyled.io <commits@restyled.io>
2020-02-29 17:41:23 +0000

    Restyled by prettier

--- src/shared/components/Search/SearchInput.js
+++ src/shared/components/Search/SearchInput.js
@@ -1,16 +1,15 @@
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
-import {UserOutlined} from '@ant-design/icons';
-import {Col, Input, Row} from 'antd';
-import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import { UserOutlined } from "@ant-design/icons";
+import { Col, Input, Row } from "antd";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
 
 export default class SearchInput extends Component {
-
-  static defaultProps = {placeholder : "GitHub @username"};
+  static defaultProps = { placeholder: "GitHub @username" };
 
   static propTypes = {
-    onSubmit : PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
     placeholder: PropTypes.string.isRequired
   };
 
@@ -21,12 +20,20 @@ export default class SearchInput extends Component {
 
   render() {
     return (
-      <Row style={{
-      marginTop: 15}}>
+      <Row
+        style={{
+          marginTop: 15
+        }}
+      >
         <Col offset={8} span={8}>
           <Input.Search
-    prefix = {<UserOutlined style={{
-        color: 'rgba(0,0,0,.25)' }} />}
+            prefix={
+              <UserOutlined
+                style={{
+                  color: "rgba(0,0,0,.25)"
+                }}
+              />
+            }
             size="large"
             enterButton
             allowClear
@@ -36,5 +43,5 @@ export default class SearchInput extends Component {
         </Col>
       </Row>
     );
-    }
   }
+}
--- src/shared/components/Table/Table.js
+++ src/shared/components/Table/Table.js
@@ -1,48 +1,70 @@
-import {RightOutlined} from '@ant-design/icons';
-import {Avatar, Button, Col, Row, Table as AntTable, Tag} from 'antd';
-import React from 'react';
-import {Link} from "react-router-dom";
+import { RightOutlined } from "@ant-design/icons";
+import { Avatar, Button, Col, Row, Table as AntTable, Tag } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Table extends React.Component {
   render() {
-    const {data} = this.props;
+    const { data } = this.props;
     return (
-        <Row><Col offset = {5} span = {14}><div style = {{ paddingTop: 40 }}>
-        <AntTable columns = {columns} dataSource = { data } />
+      <Row>
+        <Col offset={5} span={14}>
+          <div style={{ paddingTop: 40 }}>
+            <AntTable columns={columns} dataSource={data} />
           </div>
         </Col>
-      </Row>)
+      </Row>
+    );
   }
 }
 
-const columns = [{
-  title: '',
-  dataIndex: 'image_url',
-  key: 'image_url',
-  render: src => <Avatar src={src} alt="user image"/>
-}, {
-  title: 'Name', dataIndex: 'info', key: 'info',
-      render: info =>
-          <a target = "_blank" rel =
-               "noopener noreferrer" href = {info.link}>{info.name}<
-              /a>,
-}, {
-  title: 'Score',
-  dataIndex: 'score',
-  key: 'score',
-  render: score =>
-    <span>
-      <Tag color={score >= 75 ? 'green' : score >= 50 ? 'geekblue' : 'volcano'} key={score}>{score}</Tag><
-          /span>
-},{
-  title: '',
-  dataIndex: 'moreInfo',
-  key: 'moreInfo',
-  render: moreInfo =>
-    <Button type="primary">
-      <Link to={`users/$ {
+const columns = [
+  {
+    title: "",
+    dataIndex: "image_url",
+    key: "image_url",
+    render: src => <Avatar src={src} alt="user image" />
+  },
+  {
+    title: "Name",
+    dataIndex: "info",
+    key: "info",
+    render: info => (
+      <a target="_blank" rel="noopener noreferrer" href={info.link}>
+        {info.name}
+      </a>
+    )
+  },
+  {
+    title: "Score",
+    dataIndex: "score",
+    key: "score",
+    render: score => (
+      <span>
+        <Tag
+          color={score >= 75 ? "green" : score >= 50 ? "geekblue" : "volcano"}
+          key={score}
+        >
+          {score}
+        </Tag>
+      </span>
+    )
+  },
+  {
+    title: "",
+    dataIndex: "moreInfo",
+    key: "moreInfo",
+    render: moreInfo => (
+      <Button type="primary">
+        <Link
+          to={`users/$ {
     moreInfo
   }
-`}>More Info <RightOutlined /></Link>
-    </Button>
-}];
+`}
+        >
+          More Info <RightOutlined />
+        </Link>
+      </Button>
+    )
+  }
+];
